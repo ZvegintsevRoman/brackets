42b406a598e4a7c5f129b9d4d356c6c2656693c5
ZvegintsevRoman <zvegintsev.roma@yandex.by>
2019-10-05 20:14:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  couples = bracketsConfig.map(item => item.join(''));
+  let before = 1;
+  let after = 0; 
+  
+  while (before > after) { 
+    before = str.length;
+    couples.forEach(item => {      
+      str = str.replace(item, '');
+    });
+    after = str.length;
+  }  
+
+  return !str.length;
+  
 }
